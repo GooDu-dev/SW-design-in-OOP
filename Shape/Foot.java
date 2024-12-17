a6ef9220cb951383f0001253a9cbf2675a0217d1
GooDuDev <mokumokutv@gmail.com>
2024-12-17 22:00:18 +0700

    BREAKING CHANGE: remove the dependencies between Foot.java and Ellipse.java and Rectangle.java, and add composition between Foot.java and Footshape.java

--- Shape/Foot.java
+++ Shape/Foot.java
@@ -1,17 +1,34 @@
+import java.util.Scanner;
+
 public class Foot {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("What to draw? 1. Ellipse, 2. Rectangle ");
+        String result = scan.nextLine();
+
+        try {
+            Integer.parseInt(result);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input");
+        }
+
+        int id = Integer.parseInt(result);
+        draw(id);
+
+        scan.close();
     }
 
-    public void draw(Object obj) {
-        switch (obj.getClass().getName()) {
-            case "Ellipse":
-                Ellipse e = new Ellipse();
-                e.draw();
+    public static void draw(int id) {
+
+        FootShape fs = new FootShape();
+
+        switch (id) {
+            case 1:
+                fs.drawAsEllipse();
                 break;
-            case "Rectangle":
-                Rectangle r = new Rectangle();
-                r.draw();
+            case 2:
+                fs.drawAsRectangle();
                 break;
         }
     }
