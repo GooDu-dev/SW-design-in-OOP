49bfaf7d9ba37c55155708c29cf5639a4b6235dc
GooDuDev <mokumokutv@gmail.com>
2025-01-10 12:32:20 +0700

    [Good] feat: init exercise

---
+++ 5_3/CustomerMailFactory.java
@@ -0,0 +1,22 @@
+public class CustomerMailFactory {
+    public static Customer createCustomer(String type) throws ExceptionInInitializerError {
+        Customer output = null;
+        switch (type) {
+            case "Regular":
+                output = new RegularCustomer();
+                break;
+            case "Mountain":
+                output = new MountainCustomer();
+                break;
+            case "Delinquent":
+                output = new DelinquentCustomer();
+                break;
+        }
+
+        if (output == null) {
+            throw new ExceptionInInitializerError();
+        }
+
+        return output;
+    }
+}
